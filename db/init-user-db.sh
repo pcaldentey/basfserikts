cadaf023d879f64bb84e93ee24bec988b82cfcb0
Pablo Caldentey <pablo@caldentey.org>
2020-11-12 09:22:19 +0100

    db simplified

--- db/init-user-db.sh
+++ db/init-user-db.sh
@@ -5,15 +5,11 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     CREATE SEQUENCE serial_patent;
     CREATE TABLE patent (
         id        integer PRIMARY KEY DEFAULT nextval('serial_patent'),
-        title       varchar(80) NOT NULL,
-        application_year   date,
+        title   varchar(1024) NOT NULL,
+        doc_number   varchar(32) NOT NULL,
+        application_year  integer,
         abstract text NOT NULL,
-        description text NOT NULL
-    );
-
-    CREATE SEQUENCE serial_ne;
-    CREATE TABLE named_entity (
-        id    integer PRIMARY KEY DEFAULT nextval('serial_ne'),
-         name  varchar(80) NOT NULL CHECK (name <> '')
+        description text NOT NULL,
+        named_entities text[]
     );
 EOSQL
